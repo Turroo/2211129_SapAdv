0f62d1e6220393d4ca816da86398fce557f7e9a9
Turro <federico.turrini2@studio.unibo.it>
2025-03-03 18:13:40 +0100

    added some pre files

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://username:password@localhost/sapienza_advisor"
+DATABASE_URL = "postgresql://postgres:admin@localhost/sapienza_advisor"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
